6d7c0be25fd80fdbb144f1d6ddc061866a065057
Kazeem-Bello <bel.kazeem@gmail.com>
2025-10-13 05:13:12 +0100

    model evaluation completed

---
+++ src/model/demo.py
@@ -0,0 +1,23 @@
+
+import yaml
+import os
+
+def load_params(params_path: str) -> dict:
+    with open(params_path, "r") as f:
+        params = yaml.safe_load(f)
+    return params
+
+
+def get_root_directory() ->str:
+    """get the root directory (two level up from the script location)"""
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.abspath(os.path.join(cur_dir, "../../"))
+
+# load parameter from yaml file
+root_dir = get_root_directory()
+params = load_params(params_path = os.path.join(root_dir, "params.yaml"))
+print(type(params))
+
+# log parameters
+for key, value in params.items():
+    print(key, value)
